ed205bd5da6c7a2df73ad4326825de42cf686a85
jasonhui <huihangkin@gmail.com>
2016-10-28 22:12:35 +0800

    test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for f in `find . -type f -name '*.yaml'`
 do
-    echo $f
+    aws cloudformation validate-template --template-body file:////`pwd`//$f
 done
 # target="/home/personal/scripts/07_22_13/ford/$1"
 # let count=0
