067f6dc37d82a5ff14954131d2b820368cdc0e02
jasonhui <huihangkin@gmail.com>
2016-10-28 21:47:57 +0800

    test.sh

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for f in `find . -type f -name '*.yaml'`
+do
+    echo $f
+done
+# target="/home/personal/scripts/07_22_13/ford/$1"
+# let count=0
+# for f in "$target"/*
+# do
+#     echo $(basename $f)
+#     let count=count+1
+# done
+# echo ""
+# echo "Count: $count"
